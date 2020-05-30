c711afc2ba02f963453ab658a965039ef1c6c15c
Shannon Draeker <srdraeker@gmail.com>
2020-05-29 18:45:57 -0600

    Complete Drawing Functionality

---
+++ MemeThing/Controllers/View Controllers/CanvasView.swift
@@ -0,0 +1,74 @@
+//
+//  CanvasView.swift
+//  MemeThing
+//
+//  Created by Shannon Draeker on 5/29/20.
+//  Copyright © 2020 Shannon Draeker. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    
+    // MARK: - Properties
+    
+    var lines: [[CGPoint]] = [[]]
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Drawing Methods
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        // Draw each line in the array of lines
+        for points in lines {
+            // Draw each line by using its array of points
+            context.addLines(between: points)
+            
+            // Set the defaults for the appearance of the line
+            context.setLineWidth(10.0)
+            context.setLineCap(.round)
+            
+            // Draw the path
+            context.strokePath()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first?.location(in: self),
+            var lastPoint = lines.popLast()
+            else { return }
+        
+        // Add the new points to the most recent line and save that back to the array
+        lastPoint.append(touch)
+        lines.append(lastPoint)
+        
+        // Update the display
+        setNeedsDisplay()
+    }
+    
+    // MARK: - Helper Methods
+    
+    func undoDraw() {
+        // Remove the most recent line from the array
+        _ = lines.popLast()
+        
+        // Update the display
+        setNeedsDisplay()
+    }
+}
--- MemeThing/Controllers/View Controllers/DrawingViewController.swift
+++ MemeThing/Controllers/View Controllers/DrawingViewController.swift
@@ -12,13 +12,7 @@ class DrawingViewController: UIViewController {
     
     // MARK: - Outlets
     
-    @IBOutlet weak var canvasView: UIView!
-    @IBOutlet weak var drawingImageView: UIImageView!
-    
-    // MARK: - Properties
-    
-    var lastPoint = CGPoint.zero
-    var swiped = false
+    @IBOutlet weak var canvasView: CanvasView!
     
     // MARK: - Lifecycle Methods
     
@@ -29,55 +23,9 @@ class DrawingViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func undoButtonTapped(_ sender: UIButton) {
+        canvasView.undoDraw()
     }
     
     @IBAction func sentButtonTapped(_ sender: UIButton) {
-    }
-    
-    // MARK: - Drawing Methods
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        swiped = false
-        lastPoint = touch.location(in: canvasView)
-        print("got here to \(#function) and last point is \(lastPoint)")
-    }
-    
-    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
-        print("got here to \(#function)")
-       
-        UIGraphicsBeginImageContext(canvasView.frame.size)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
-        drawingImageView.image?.draw(in: canvasView.bounds)
-        
-        context.move(to: fromPoint)
-        context.addLine(to: toPoint)
-        
-        context.setLineCap(.round)
-        context.setBlendMode(.normal)
-        context.setLineWidth(10.0)
-        context.setStrokeColor(UIColor.black.cgColor)
-        
-        context.strokePath()
-        
-        drawingImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        print("got here to \(#function) and last point is \(lastPoint)")
-        swiped = true
-        let currentPoint = touch.location(in: canvasView)
-        drawLine(from: lastPoint, to: currentPoint)
-        
-        lastPoint = currentPoint
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("got here to \(#function) and swiped is \(swiped) and last point is \(lastPoint)")
-        if !swiped { drawLine(from: lastPoint, to: lastPoint) }
     }
 }
