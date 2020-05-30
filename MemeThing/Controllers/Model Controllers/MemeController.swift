890b240550e8bcdb896e1008136dbbc320dbb944
Shannon Draeker <srdraeker@gmail.com>
2020-05-29 21:06:50 -0600

    Basic Meme Model and Controller

---
+++ MemeThing/Controllers/Model Controllers/MemeController.swift
@@ -0,0 +1,21 @@
+//
+//  MemeController.swift
+//  MemeThing
+//
+//  Created by Shannon Draeker on 5/29/20.
+//  Copyright © 2020 Shannon Draeker. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import UIKit.UIImage
+
+class MemeController {
+    
+    // MARK: - Singleton
+    
+    // MARK: - CRUD Methods
+    
+    // MARK: - Notifications
+    
+}
